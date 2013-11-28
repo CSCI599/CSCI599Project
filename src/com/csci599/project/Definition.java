1a8a77975fd4c4d5dbfefdde7d8b4914a04811db
Abdulmajeed <alameer@usc.edu>
2013-11-27 18:55:04 -0800

    add new files

---
+++ src/com/csci599/project/Definition.java
@@ -0,0 +1,19 @@
+package com.csci599.project;
+
+public class Definition {
+	
+	private int line;
+	private String varName;
+	private boolean outsideDef;
+	
+	public int getLine() {
+		return line;
+	}
+	public String getVarName() {
+		return varName;
+	}
+	public boolean isOutsideDef() {
+		return outsideDef;
+	}
+	
+}
--- src/com/csci599/project/Nodes.java
+++ src/com/csci599/project/Nodes.java
@@ -1,6 +1,8 @@
+
 package com.csci599.project;
 
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 import org.apache.bcel.generic.InstructionHandle;
 
@@ -8,10 +10,22 @@ public class Nodes {
 	public InstructionHandle nodeName;
 	public ArrayList<Nodes> parents;
 	public ArrayList<Nodes> children;
+	
+	//used to compute reaching def. info
+	public TreeSet<Definition> in;
+	public TreeSet<Definition> out;
+	public TreeSet<Definition> gen;
+	public TreeSet<Definition> kill;
+
 
 	Nodes(InstructionHandle nodeName) {
 		this.nodeName = nodeName;
 		this.parents = new ArrayList<Nodes>();
 		this.children = new ArrayList<Nodes>();
+		this.in = new TreeSet<Definition>();
+		this.out = new TreeSet<Definition>();
+		this.gen = new TreeSet<Definition>();
+		this.kill = new TreeSet<Definition>();
+
 	}
 }
